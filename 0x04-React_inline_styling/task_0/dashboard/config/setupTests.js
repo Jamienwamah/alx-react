132d9f7d7d8de0862af0cf6e1a6af2eb5a9f9de0
oishbdh <ikechukwuarinze614@gmail.com>
2023-05-30 09:51:52 +0100

    inline_style

--- 0x04-React_inline_styling/task_0/dashboard/config/setupTests.js
+++ 0x04-React_inline_styling/task_0/dashboard/config/setupTests.js
@@ -1,6 +1,4 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-// const enzyme = require('enzyme');
-// const ad = require('enzyme-adapter-react-16')
 
 configure({ adapter: new Adapter() });
